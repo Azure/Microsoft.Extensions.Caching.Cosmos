cbf5465beff0e6c15bed2cfe04bce6a57672d39a
Matias Quaranta <ealsur@hotmail.com>
2020-07-27 14:27:48 -0700

    More comments

--- src/CosmosCacheOptions.cs
+++ src/CosmosCacheOptions.cs
@@ -53,8 +53,9 @@ namespace Microsoft.Extensions.Caching.Cosmos
         /// </summary>
         /// <remarks>
         /// <para>This can be useful for applications with high frequency reads on the same cache item that does not change.</para>
-        /// <para>Turning this on for cache items that change constantly can lead to dirty reads. </para>
+        /// <para>Turning this feature off for cache items that change constantly can lead to dirty reads. </para>
         /// </remarks>
+        /// <value>Default value is true</value>
         public bool RetrySlidingExpirationUpdates { get; set; } = true;
 
         /// <summary>
