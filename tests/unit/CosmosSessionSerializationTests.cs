cf21159588933b34d0dd99ae117a519e2a97802a
Matias Quaranta <ealsur@hotmail.com>
2020-10-12 17:36:44 -0700

    tests

--- tests/unit/CosmosSessionSerializationTests.cs
+++ tests/unit/CosmosSessionSerializationTests.cs
@@ -27,6 +27,8 @@ namespace Microsoft.Extensions.Caching.Cosmos.Tests
             existingSession.Content = new byte[0];
             string serialized = JsonConvert.SerializeObject(existingSession);
             Assert.False(serialized.Contains("\"ttl\""), "Session without expiration should not include ttl property.");
+            Assert.False(serialized.Contains("\"absoluteSlidingExpiration\""), "Session without expiration should not include absoluteSlidingExpiration property.");
+            Assert.False(serialized.Contains("\"isSlidingExpiration\""), "Session without expiration should not include isSlidingExpiration property.");
         }
 
         [Fact]
@@ -42,13 +44,36 @@ namespace Microsoft.Extensions.Caching.Cosmos.Tests
         }
 
         [Fact]
+        public void ValidatesAbsoluteSlidingExpirationDoesSerializeProperty()
+        {
+            CosmosCacheSession existingSession = new CosmosCacheSession();
+            existingSession.SessionKey = "key";
+            existingSession.Content = new byte[0];
+            existingSession.AbsoluteSlidingExpiration = 10;
+            string serialized = JsonConvert.SerializeObject(existingSession);
+            Assert.True(serialized.Contains("\"absoluteSlidingExpiration\""), "Session with absolute sliding expiration should include absoluteSlidingExpiration property.");
+        }
+
+        [Fact]
+        public void ValidatesIsSlidingExpirationDoesSerializeProperty()
+        {
+            CosmosCacheSession existingSession = new CosmosCacheSession();
+            existingSession.SessionKey = "key";
+            existingSession.Content = new byte[0];
+            existingSession.IsSlidingExpiration = true;
+            string serialized = JsonConvert.SerializeObject(existingSession);
+            Assert.True(serialized.Contains("\"isSlidingExpiration\""), "Session with sliding expiration should include isSlidingExpiration property.");
+        }
+
+        [Fact]
         public void ValidatesContract()
         {
-            const string expectedContract = "{\"id\":\"key\",\"content\":\"AQ==\",\"ttl\":5,\"isSlidingExpiration\":true}";
+            const string expectedContract = "{\"id\":\"key\",\"content\":\"AQ==\",\"ttl\":5,\"isSlidingExpiration\":true,\"absoluteSlidingExpiration\":10}";
             CosmosCacheSession existingSession = new CosmosCacheSession();
             existingSession.SessionKey = "key";
             existingSession.IsSlidingExpiration = true;
             existingSession.TimeToLive = 5;
+            existingSession.AbsoluteSlidingExpiration = 10;
             existingSession.Content = new byte[1] { 1 };
             string serialized = JsonConvert.SerializeObject(existingSession);
             Assert.Equal(expectedContract , serialized);
